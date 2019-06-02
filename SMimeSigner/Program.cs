49b2d22b530c9be67c27e01e45ceac45073180fd
Glenn Watson <github@glennwatson.net>
2019-06-02 17:24:25 +1000

    First commit

---
+++ SMimeSigner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SMimeSigner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
